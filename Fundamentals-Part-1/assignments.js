5ee5869f33956d60701a23c252dbee0078e3cabe
Bogdan <thegarrybit@gmail.com>
2023-03-09 22:40:04 +0200

    feat: add basic operators and complete assignment

--- Fundamentals-Part-1/assignments.js
+++ Fundamentals-Part-1/assignments.js
@@ -8,3 +8,12 @@ let language;
 console.log(country, population, isIsland, language);
 
 language = 'Ukrainian';
+
+const populationHalf = population / 2;
+console.log(population++);
+const finladPopulation = 6000000
+console.log(population > finladPopulation);
+console.log(population < 33000000);
+
+const description = `${country} is in ${continet}, and its ${population} people speak ${language}`;
+console.log(description);
