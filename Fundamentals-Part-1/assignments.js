900d6522735f0e43a38a21b969185ffef462b6ff
Bogdan <thegarrybit@gmail.com>
2023-03-09 20:35:31 +0200

    feat: complete assignment

--- Fundamentals-Part-1/assignments.js
+++ Fundamentals-Part-1/assignments.js
@@ -2,4 +2,7 @@ let country = 'Ukraine';
 let continet = 'Europe';
 let population = 43031753;
 
-console.log(country, continet, population);
+let isIsland = false;
+let language;
+
+console.log(country, population, isIsland, language);
