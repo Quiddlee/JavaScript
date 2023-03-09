18511d77fa53b761a275d304fa736dbd0442ec13
Bogdan <thegarrybit@gmail.com>
2023-03-09 20:09:56 +0200

    feat: add new note files

---
+++ Fundamentals-Part-1/assignments.js
@@ -0,0 +1,5 @@
+let country = 'Ukraine';
+let continet = 'Europe';
+let population = 43031753;
+
+console.log(country, continet, population);
---
+++ Fundamentals-Part-1/script.js
@@ -0,0 +1,26 @@
+let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+let first = 'Jonas';
+let firstNamePerson;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+let PI = 3.1415;
+
+let myfirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myfirstJob);
